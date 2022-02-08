58f10d43d66d4ce0b79a0048c3f3f2b80f5d0ede
Ivan <denatarelcom2105@gmail.com>
2022-02-08 21:20:20 +0200

    lecture_1 homework task_4 finished

---
+++ 01_lecture/tasks/task_4.js
@@ -0,0 +1,16 @@
+// Create a function searchSerial, which takes two parameters: 1 - an array of string or numeric values, 2 - searched value. 
+// The function should return a number - maximum count of serial entries of the given value.
+
+const searchSerial = (arr, searchVal) => {
+    let num = 0;
+    if(!arr.includes(searchVal)) return num
+
+    for(let item of arr) {
+        if(item === searchVal) num++
+    }
+
+    return num
+}
+
+console.log(searchSerial([1, 2, 2, 3, 4, 5, 2], 2));
+console.log(searchSerial([1, 2, 2, 3, 4, 5, 2], 10));
